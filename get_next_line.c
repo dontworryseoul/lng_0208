e47d9f52c457cbf04aa7386c3264447d58bc7dc2
jso <jso@student.42seoul.kr>
2021-02-11 15:39:37 +0900

    add OPEN_MAX Array

--- get_next_line.c
+++ get_next_line.c
@@ -25,23 +25,23 @@ int find_return(int fd, char **backup)
         else if(read_len == 0)
             break ;
         buf[read_len] = '\0';
-        //c1.loop1
-        //buf = ABCDE\0
-        //c1.loop2
-        //buf = FG\nAB\0
+            //c1.loop1
+            //buf = ABCDE\0
+            //c1.loop2
+            //buf = FG\nAB\0
 
         tmp = ft_strjoin(*backup, buf);//strjoin은 매개변수가 null일 경우도 동작
-        //c1.loop1
-        //tmp = ABCDE\0
-        //c1.loop2
-        //tmp = ABCDEFG\nAB\0
+            //c1.loop1
+            //tmp = ABCDE\0
+            //c1.loop2
+            //tmp = ABCDEFG\nAB\0
         if (*backup)
             free(*backup);
         *backup = tmp;
-        //c1.loop1
-        //*backup = ABCDE\0
-        //c1.loop2
-        //*backup = ABCDEFG\nAB\0
+            //c1.loop1
+            //*backup = ABCDE\0
+            //c1.loop2
+            //*backup = ABCDEFG\nAB\0
     }
     free(buf);
     return (1);
@@ -50,7 +50,6 @@ int find_return(int fd, char **backup)
     //
     //return ();
 }
-
 char *set_after_nl(char **backup)
 {
     // 받아온 문장을 strlen 으로 전체 길이를 알아내고
@@ -64,7 +63,6 @@ char *set_after_nl(char **backup)
     free(*backup);
     return (tmp);
 }
-
 char *set_prev_nl(char *backup)
 {
     char *str;
@@ -81,35 +79,35 @@ char *set_prev_nl(char *backup)
     return (tmp);
 }
 
-//case 1
-//ABCDEFG\n BUFFER_SIZE = 5
-//ABC
+    //case 1
+    //ABCDEFG\n BUFFER_SIZE = 5
+    //ABC
 
-//case 2
-//ABCDEFG\n BUFFER_SIZE = 10
-//ABC
+    //case 2
+    //ABCDEFG\n BUFFER_SIZE = 10
+    //ABC
 
-//case 3
-//
+    //case 3
+    //
 
 int get_next_line(int fd, char **line)
 {
-    static char *backup;
+    static char *backup[OPEN_MAX + 1];
     char *tmp;
     if (fd < 0 && line == 0 && BUFFER_SIZE <= 0)
         return (-1);
-    if (find_return(fd, &backup) == -1)
+    if (find_return(fd, &backup[fd]) == -1)
         return (-1);
 
-// while 필요한 함수
-// 1. '\n' 전까지 별도 문장으로 만들어 주는 함수.
-    if (!(*line = set_prev_nl(backup)))
+    // while 필요한 함수
+    // 1. '\n' 전까지 별도 문장으로 만들어 주는 함수.
+    if (!(*line = set_prev_nl(backup[fd])))
         return (-1);
-// 2. '\n' 후부터 별도 문장으로 만들어 주는 함수.
-    if (!(backup = set_after_nl(&backup)))
+    // 2. '\n' 후부터 별도 문장으로 만들어 주는 함수.
+    if (!(backup = set_after_nl(&backup[fd])))
         return (-1);
     return (1);//마지막행일때 0과같이 출력하는 예외처리를 어디서 해줄지 고민하기.
 }
-//case1 res1 
-//line -> ABCDEFG\0
-//backup ->AB\0
+    //case1 res1 
+    //line -> ABCDEFG\0
+    //backup ->AB\0
