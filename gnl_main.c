e7c2074eafec66a6a1d80ff78c0c7c1a3a7262d5
jso <jso@student.42seoul.kr>
2021-02-11 22:28:15 +0900

    print one line

--- gnl_main.c
+++ gnl_main.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include "get_next_line.h"
 #include <fcntl.h>
+void only_free(char *str)
+{
+    if (str)
+        free(str);
+    str = 0;
+}
 
 int get_next_line(int fd, char **line)
 {
     static char *backup[OPEN_MAX + 1];
-    char        *tmp;
     char        *buf;
-    int         ret;
     int         read_len;
+    char        *tmp;
+    // int         ret;
 
     if (fd < 0 && line == 0 && BUFFER_SIZE <= 0)
         return (-1);
-    buf = malloc(sizeof(char) * (BUFFER_SIZE + 1)
+    buf = malloc((sizeof(char) * (BUFFER_SIZE + 1)));
     if (!buf)
         return (-1);
     read_len = read(fd, buf, BUFFER_SIZE);
     buf[read_len] = '\0';
     if (read_len == 0)
         return (0);
-    *line = ft_strjoin(backup, buf);
+    if (!backup[fd])
+        backup[fd] = ft_strdup("");
+    tmp = backup[fd];
+    *line = ft_strjoin(backup[fd], buf);
+    only_free(tmp);
     free(buf);
     return (1);    
 }
@@ -28,12 +38,12 @@ int main(void)
 {
     int fd;
     char *line;
-    char buf[BUFFER_SIZE + 1];
-    int num;
 
     fd = open("text.txt", O_RDONLY);
     get_next_line(fd, &line);
     puts(line);
+    // get_next_line(fd, &line);
+    // puts(line);
     free(line);
     close (fd);
     return (0);
