af5faf225883717b0b7cd048afcebc08e37ea3d6
lky <HelloWorld@jeonminhoui-MacBook-Pro.local>
2021-02-13 15:59:42 +0900

    a

--- get_next_line.c
+++ get_next_line.c
@@ -8,8 +8,6 @@ int find_return(int fd, char **backup)
 
 	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 		return (-1);
-        // backup  맨앞서부터 검사하는가?
-        // backup 앞에서 이미 \n이 없는걸 아는데 계속 처음부터 다시 검사하는게 비효율적임
     while (!(ft_strchr(*backup, '\n')))
     {
         if ((read_len = read(fd, buf, BUFFER_SIZE)) == -1)
@@ -18,53 +16,20 @@ int find_return(int fd, char **backup)
             buf = 0;
             return (-1);
         }
-        //c1.1-loop1.1
-        //buf = ABCDE
-        //c1.1-loop1.2
-        //buf = FG\nAB
-        //c1.1-loop2.1
-        //buf = C
         else if(read_len == 0)
             break ;
         buf[read_len] = '\0';
-        //c1.loop1.1
-        //buf = ABCDE\0
-        //c1.loop1.2
-        //buf = FG\nAB\0
-        //c1.1-loop2.1
-        //buf = C\0
-
-        tmp = ft_strjoin(*backup, buf);//strjoin은 매개변수가 null일 경우도 동작
-        //c1.loop1.1
-        //tmp = ABCDE\0
-        //c1.loop1.2
-        //tmp = ABCDEFG\nAB\0
-        //c1.loop2.1
-        //tmp = ABC\0
+        tmp = ft_strjoin(*backup, buf);
         if (*backup)
             free(*backup);
         *backup = tmp;
-        //c1.loop1.1
-        //*backup = ABCDE\0
-        //c1.loop1.2
-        //*backup = ABCDEFG\nAB\0
-        //c1.loop2.1
-        //*backup = ABC\0
     }
     free(buf);
     return (1);
-    //while문에 나온 값은 '\n'을 포함한 문장. 
-    //개행문자가 있거나 혹은 eof에 도달한 경우
-    //
-    //return ();
 }
 
 char *set_after_nl(char **backup, int *flagcheck)
 {
-    // 받아온 문장을 strlen 으로 전체 길이를 알아내고
-    // 받아온 문장 '\n' 까지의 길이를 알아내고
-    // malloc 문장 길이 - 개행까지 길이 + 1 
-    // '\n' 개행 뒤 문장을 위에서 할당 받은 메모리에 할당 후 반환.
     char *tmp;
 
     tmp = 0;
@@ -72,8 +37,6 @@ char *set_after_nl(char **backup, int *flagcheck)
         tmp = ft_strdup(ft_strchr(*backup, '\n') + 1);
     else
         *flagcheck = 0;
-    //c1.loop1
-    //tmp = AB\0
     free(*backup);
     return (tmp);
 }
@@ -81,37 +44,13 @@ char *set_after_nl(char **backup, int *flagcheck)
 char *set_prev_nl(char *backup)
 {
     char *tmp;
-    // 받아온 문장에 '\n' 까지 읽고, 그 길이 +1만큼 malloc으로 메모리 공간 할당
-    // 할당한 영역에 문장 처음부터 '\n'까지 넣기.
-    // 만들어진 문장을 반환하기.
+
     tmp = ft_strdup(backup);
-    //c1.loop1
-    //tmp = ABCDEFG\nAB\0
-    //c1.loop2
-    //tmp = ABC\0
-    //*(ft_strchr(tmp, '\n')) = '\0'; //ERR:strchr이 NULL포인터일때 문제가생김...
-    
     if((ft_strchr(tmp, '\n')))
         *(ft_strchr(tmp, '\n')) = '\0';
-    
-    //c1.loop1
-    //tmp = ABCDEFG\0
-    //c1.loop2
-    //ERR
     return (tmp);
 }
 
-//case 1 -->ERR LINE88
-//ABCDEFG\n BUFFER_SIZE = 5
-//ABC
-
-//case 2 -->ERR LINE88
-//ABCDEFG\n BUFFER_SIZE = 10
-//ABC
-
-//case 3 -->ERR LINE88
-//ABCDEFG\n BUFFER_SIZE = 8
-//ABC
 #include <stdio.h>
 int get_next_line(int fd, char **line)
 {
@@ -124,16 +63,9 @@ int get_next_line(int fd, char **line)
         return (-1);
     if (!flagcheck)
         return (0);
-
-// while 필요한 함수
-// 1. '\n' 전까지 별도 문장으로 만들어 주는 함수.
     if (!(*line = set_prev_nl(backup)))
         return (0);
-
     if (!(backup = set_after_nl(&backup, &flagcheck)))
         return (0);
-    return (flagcheck);//마지막행일때 0과같이 출력하는 예외처리를 어디서 해줄지 고민하기.
+    return (flagcheck);
 }
-//case1 res1
-//line -> ABCDEFG\0
-//backup ->AB\0
