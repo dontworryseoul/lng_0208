fbd991dbbe94a8904157e8512c80bb5f0445c9f5
lky <HelloWorld@jeonminhoui-MacBook-Pro.local>
2021-02-13 16:28:42 +0900

    a

--- get_next_line.c
+++ get_next_line.c
@@ -96,7 +96,7 @@ int get_next_line(int fd, char **line)
 {
     static char *backup;
     char *tmp;
-    if (fd < 0 && line == 0 && BUFFER_SIZE <= 0)
+    if (fd < 0 || line == 0 || BUFFER_SIZE <= 0)
         return (-1);
     if (find_return(fd, &backup) == -1)
         return (-1);
